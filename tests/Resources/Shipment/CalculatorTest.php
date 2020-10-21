c2db0c6ae0b6a353602a9564a1b0260582e87f82
Pedro Barros <pedro.barros@melhorenvio.com>
2020-10-21 16:26:57 -0300

    primeiro documento de testes

---
+++ tests/Resources/Shipment/CalculatorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MelhorEnvio\Tests\Resources\Shipment;
+
+use GuzzleHttp\ClientInterface;
+use MelhorEnvio\Resources\Resource;
+use MelhorEnvio\Resources\Shipment\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     */
+    public function is_valid_calculate()
+    {
+        $response = $this->createMock(Calculator::class);
+        $response->method('getPayload');
+
+        $clientStub = $this->createMock(ClientInterface::class);
+
+        $clientStub->method('send')->willReturn($response);
+
+        $resourceStub = $this->createMock(Resource::class);
+        $resourceStub->method('getHttp')->willReturn($clientStub);
+
+    }
+
+    /**
+     * @test
+     */
+    public function is_invalid_calculate()
+    {
+        $clientStub = $this->createMock(ClientInterface::class);
+        $clientStub->method('send')->willReturn(function () { throw new \Exception; });
+
+        $resourceStub = $this->createMock(Resource::class);
+        $resourceStub->method('getHttp')->willReturn($clientStub);
+    }
+}
